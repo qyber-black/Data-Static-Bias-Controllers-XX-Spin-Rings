ef9307a7bdd130fa9b1efa6cbc45af587f9089bb
Sean O'Neil <seanonei@usc.edu>
2022-11-21 09:07:10 -0800

    new script - creates gell-mann basis matrices

---
+++ matlab/bloch_basis.m
@@ -0,0 +1,49 @@
+%% Produce Bloch Basis Only One Time Per Iteration
+
+function [b]=bloch_basis(N,I);
+
+% Produce Natrual Basis Vectors
+for j=1:N
+    e{j}=I(:,j);
+end
+
+% Produce Diagonal Basis Matrices 
+for k=1:(N-1)
+    b{k}=zeros(N,N);
+    for l=1:k
+        b{k}=b{k}+e{l}*e{l}';
+    end
+    b{k}=1/sqrt(k+k^2)*(b{k}-k*e{k+1}*e{k+1}');
+end
+
+% Re-Index Diagonal Basis Matrices
+for k=1:(N-1)
+    b{N^2-N+k}=b{k};
+end
+
+b{N^2}=(1/sqrt(N))*eye(N,N);
+
+% Produce Real Symmetric Off-Diagonal Basis Matrices 
+z=1;
+for k=1:N
+    for l=(k):(N-1)
+        b{z}=1/sqrt(2)*(e{k}*e{l+1}'+e{l+1}*e{k}');
+        z=z+1;
+    end
+end
+
+% Produce Imaginary Hermitian Off-Diagonal Basis Matrices
+for k=1:N
+    for l=(k):(N-1)
+        b{z}=1/sqrt(2)*(i*e{k}*e{l+1}'-i*e{l+1}*e{k}');
+        z=z+1;
+    end
+end
+
+% Test Orthogonality of Basis Matrices
+for j=1:N^2
+    for k=1:N^2
+        O(j,k)=trace(b{j}*b{k});
+    end
+end
+
